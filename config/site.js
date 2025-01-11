a9822e9e2af04af15e65503962e2fc7568931b6e
igloo-9 <ikcyr@hotmail.com>
2025-01-11 12:47:26 -0800

    build: change the meta data of the site with a more thorough skeleton

---
+++ config/site.js
@@ -0,0 +1,15 @@
+export const siteConfig = {
+  name: 'ricky/platform',
+  url: 'https://ricky.platform.com',
+  ogImage: '',
+  description: 'Personal website for portfolio and blog posts mainly.',
+  links: {
+    twitter: '',
+    github: '',
+  },
+}
+
+export const META_THEME_COLORS = {
+  light: '#ffffff',
+  dark: '#09090b',
+}
