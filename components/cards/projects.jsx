b9f5ccab4e4be7454c3be57f2df5330df67663e8
igloo-9 <ikcyr@hotmail.com>
2025-01-17 18:43:00 -0800

    chore: remove redundant part

--- components/cards/projects.jsx
+++ components/cards/projects.jsx
@@ -1,9 +1,6 @@
 'use client'
 
 import { useEffect, useState } from 'react'
-import { useTheme } from 'next-themes'
-import Image from 'next/image'
-import Link from 'next/link'
 
 import { Button } from '@/components/ui/button'
 import {
@@ -15,7 +12,6 @@ import {
 } from '@/components/ui/card'
 
 export function CardsProjects() {
-  const { resolvedTheme } = useTheme()
   const [mounted, setMounted] = useState(false)
 
   useEffect(() => {
