24a65296cec4d6e6ed13de81ee6348b87dd0720c
igloo-9 <ikcyr@hotmail.com>
2025-01-18 13:26:27 -0800

    refactor: allow viewers to go back

--- components/dev-alert.jsx
+++ components/dev-alert.jsx
@@ -2,21 +2,31 @@
 
 import { Terminal } from 'lucide-react'
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
-
-import { usePathname } from 'next/navigation'
+import { Button } from '@/components/ui/button'
 
 export function DevAlert() {
-  const pathname = usePathname()
+  const handleBack = () => {
+    window.history.back()
+  }
 
   return (
-    <div className="w-fit fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-      <Alert className="pr-16">
-        <Terminal className="h-4 w-4" />
-        <AlertTitle>Sorry</AlertTitle>
-        <AlertDescription>
-          This part of the site is still under development...
-        </AlertDescription>
-      </Alert>
-    </div>
+    <>
+      <Button
+        onClick={handleBack}
+        className="fixed top-8 left-6"
+        variant="outline"
+      >
+        Back
+      </Button>
+      <div className="w-fit fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        <Alert className="pr-16">
+          <Terminal className="h-4 w-4" />
+          <AlertTitle>Sorry</AlertTitle>
+          <AlertDescription>
+            This part of the site is still under development...
+          </AlertDescription>
+        </Alert>
+      </div>
+    </>
   )
 }
